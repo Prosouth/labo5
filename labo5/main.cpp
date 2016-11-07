5b0c629855e57785149404cc82f930f02985057b
Erick Corazza <erick.corazza@gmail.com>
2016-11-07 11:13:14 +0100

    oklm

---
+++ labo5/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Erick Corazza
+ *
+ * Created on 7 novembre 2016, 11:12
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
