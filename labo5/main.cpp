3d2eace6cdc408a457ee56dba0a5853db322af86
Sebastien Saez <monsieursaez@gmail.com>
2016-11-14 11:48:43 +0100

    consistance is the key

--- labo5/main.cpp
+++ labo5/main.cpp
@@ -185,18 +185,42 @@ void affichageCentreMois(int mois, int premierJourSemaine)
     cout << setw(LARGEUR_MOIS);
     switch(mois)
     {
-        case 1 : cout << "Janvier" << endl ; break;
-        case 2 : cout << "Février" << endl ; break;
-        case 3 : cout << "Mars" << endl ; break;
-        case 4 : cout << "Avril" << endl ; break;
-        case 5 : cout << "Mai" << endl ; break;
-        case 6 : cout << "Juin"  << endl ; break;
-        case 7 : cout << "Juillet" << endl ; break;
-        case 8 : cout << "Août" << endl ; break;
-        case 9 : cout << "Septembre" << endl ; break;
-        case 10: cout << "Octobre" << endl ; break;
-        case 11: cout << "Novembre" << endl ; break;
-        case 12: cout << "Décembre" << endl ; break;
+        case 1 : 
+           cout << "Janvier" << endl;
+           break;
+        case 2 : 
+           cout << "Février" << endl;
+           break;
+        case 3 : 
+           cout << "Mars" << endl;
+           break;
+        case 4 : 
+           cout << "Avril" << endl; 
+           break;
+        case 5 : 
+           cout << "Mai" << endl; 
+           break;
+        case 6 : 
+           cout << "Juin"  << endl; 
+           break;
+        case 7 : 
+           cout << "Juillet" << endl; 
+           break;
+        case 8 : 
+           cout << "Août" << endl; 
+           break;
+        case 9 : 
+           cout << "Septembre" << endl; 
+           break;
+        case 10: 
+           cout << "Octobre" << endl; 
+           break;
+        case 11: 
+           cout << "Novembre" << endl; 
+           break;
+        case 12: 
+           cout << "Décembre" << endl; 
+           break;
     }
     
     
